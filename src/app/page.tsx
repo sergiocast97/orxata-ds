268ab8cc5d19f8f095c148912774a764f6d05c7f
Sergio <sergiocast97@gmail.com>
2025-02-06 01:00:43 +0000

    Typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,59 @@
-import Image from "next/image";
+//import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="dark">
+      <div className="min-h-screen bg-theme-white dark:bg-theme-darkest">
+        <div className="flex flex-col gap-8 xl:gap-16 xl:flex-col text-theme-darkest dark:text-theme-white">
+          <div className="flex flex-col gap-2">
+            <div className="mb-6 text-2xl font-bold md:text-4xl font-heading ">
+              Headings:
+              <span className="font-bold">Poppins</span>
+            </div>
+            <h1 className=" head-h1 ">Heading 1 - 60px</h1>
+            <h2 className=" head-h2 ">Heading 2 - 48px</h2>
+            <h3 className=" head-h3 ">Heading 3 - 36px</h3>
+            <h4 className=" head-h4 ">Heading 4 - 30px</h4>
+            <h5 className=" head-h5 ">Heading 5 - 24px</h5>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex flex-col gap-2">
+            <div className="mb-6 font-sans text-2xl font-bold md:text-4xl ">
+              Body:
+              <span className="font-normal">Inter</span>
+            </div>
+
+            <div className="flex flex-col flex-wrap gap-8 sm:flex-row sm:gap-12 lg:gap-16">
+              <div className="space-y-1">
+                <p className="body-2xl ">Body 2xl - 24px</p>
+                <p className="body-xl ">Body xl - 20px</p>
+                <p className="body-lg ">Body lg - 18px</p>
+                <p className="body-md ">Body md - 16px</p>
+                <p className="body-sm ">Body sm - 14px</p>
+                <p className="body-xs ">Body xs - 12px</p>
+              </div>
+
+              <div className="space-y-1">
+                <p className="font-semibold body-2xl ">Semi 2xl - 24px</p>
+                <p className="font-semibold body-xl ">Semi xl - 20px</p>
+                <p className="font-semibold body-lg ">Semi lg - 18px</p>
+                <p className="font-semibold body-md ">Semi md - 16px</p>
+                <p className="font-semibold body-sm ">Semi sm - 14px</p>
+                <p className="font-semibold body-xs ">Semi xs - 12px</p>
+              </div>
+
+              <div className="space-y-1">
+                <p className="font-bold body-2xl ">Bold 2xl - 24px</p>
+                <p className="font-bold body-xl ">Bold xl - 20px</p>
+                <p className="font-bold body-lg ">Bold lg - 18px</p>
+                <p className="font-bold body-md ">Bold md - 16px</p>
+                <p className="font-bold body-sm ">Bold sm - 14px</p>
+                <p className="font-bold body-xs ">Bold xs - 12px</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
