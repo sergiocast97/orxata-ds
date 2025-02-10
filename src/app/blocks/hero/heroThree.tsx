9e5a1d44da01dd9b65fb107432b191b0cf3ffde9
Sergio <sergiocast97@gmail.com>
2025-02-10 23:58:30 +0000

    hero three test

--- src/app/blocks/hero/heroThree.tsx
+++ src/app/blocks/hero/heroThree.tsx
@@ -1,3 +1,40 @@
+import Button from '@/app/components/button';
+import Image from 'next/image';
+
 export default function HeroThreeBlock() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <Image
+          width="1312"
+          height="360"
+          src="https://dummyimage.com/1312x360/98A2E7/1A1D33?text="
+          alt="Hero image"
+          className="aspect-[4/1] w-full object-cover rounded-theme-lg"
+          loading="lazy"
+        />
+        <div className="grid grid-cols-1 lg:grid-cols-2 pt-8 gap-8">
+          <div className="flex flex-col items-start gap-1">
+            <p className="body-sm font-semibold text-theme-strong">
+              Lorem Ipsum dolor sit amet
+            </p>
+            <h1 className="head-h1 text-theme-heading">
+              Impactful Headline about product
+            </h1>
+          </div>
+          <div className="flex flex-col items-start gap-8">
+            <p className="body-lg text-theme-copy max-w-[48ch]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              viverra eros bibendum sapien sagittis, ac efficitur felis
+              accumsan. Vivamus ut efficitur risus.
+            </p>
+            <Button
+              url="#"
+              text="Filled button"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import FeatureOneBlock from './blocks/feature/featureOne';
 import FeatureTwoBlock from './blocks/feature/featureTwo';
 import FooterOne from './blocks/footer/footerOne';
 import HeroOneBlock from './blocks/hero/heroOne';
+import HeroThreeBlock from './blocks/hero/heroThree';
 
 export default function Home() {
   return (
     <>
+      <HeroThreeBlock />
       <HeroOneBlock />
       <FeatureTwoBlock />
       <FeatureOneBlock />
