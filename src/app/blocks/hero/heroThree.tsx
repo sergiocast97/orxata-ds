25232952544d6709710d6a5c47b60cf1ca71b1ef
Sergio <sergiocast97@gmail.com>
2025-02-18 01:40:33 +0000

    container

--- src/app/blocks/hero/heroThree.tsx
+++ src/app/blocks/hero/heroThree.tsx
@@ -4,34 +4,36 @@ import Image from "next/image";
 
 export default function HeroThreeBlock() {
   return (
-    <>
-      <div className="container">
-        <Image
-          width="1312"
-          height="480"
-          src="https://hwhfyb8e9b.ufs.sh/f/E8jMENC68tjA4lvPTjxfshF9qjNVRickg0ZzuwdolY726EL5"
-          alt="Hero image"
-          className="aspect-[7/3] w-full object-cover rounded-theme-lg"
-          loading="lazy"
-        />
-        <div className="grid grid-cols-1 xl:grid-cols-2 pt-8 gap-8">
-          <div className="flex flex-col items-start justify-center gap-1">
-            <p className="body-sm font-semibold text-theme-strong">Lorem Ipsum dolor sit amet</p>
-            <h1 className="head-h1 text-theme-heading text-balance">
-              Impactful Headline about product
-            </h1>
-          </div>
-          <div className="flex flex-col items-start justify-center gap-8">
-            <p className="body-lg text-theme-copy max-w-[48ch]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra eros bibendum
-              sapien sagittis, ac efficitur felis accumsan. Vivamus ut efficitur risus.
-            </p>
-            <ButtonLink href="#" type="solid" theme="light">
-              Learn more <ArrowRightIcon className="size-6" />
-            </ButtonLink>
+    <div className="bg-theme-background" data-theme="dark">
+      <div className="p-6 md:p-8 lg:p-12 xl:p-20">
+        <div className="container">
+          <Image
+            width="1312"
+            height="480"
+            src="https://hwhfyb8e9b.ufs.sh/f/E8jMENC68tjA4lvPTjxfshF9qjNVRickg0ZzuwdolY726EL5"
+            alt="Hero image"
+            className="aspect-[7/3] w-full object-cover rounded-theme-lg"
+            loading="lazy"
+          />
+          <div className="grid grid-cols-1 xl:grid-cols-2 pt-8 gap-8">
+            <div className="flex flex-col items-start justify-center gap-1">
+              <p className="body-sm font-semibold text-theme-strong">Lorem Ipsum dolor sit amet</p>
+              <h1 className="head-h1 text-theme-heading text-balance">
+                Impactful Headline about product
+              </h1>
+            </div>
+            <div className="flex flex-col items-start justify-center gap-8">
+              <p className="body-lg text-theme-copy max-w-[48ch]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra eros bibendum
+                sapien sagittis, ac efficitur felis accumsan. Vivamus ut efficitur risus.
+              </p>
+              <ButtonLink href="#" type="solid" theme="light">
+                Learn more <ArrowRightIcon className="size-6" />
+              </ButtonLink>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
