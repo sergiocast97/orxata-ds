85450db1fc2a5049a55a7a9c650182ae62e30764
Sergio <sergiocast97@gmail.com>
2025-02-19 22:19:54 +0000

    Feature Two Block

--- src/app/blocks/feature/FeatureTwo.tsx
+++ src/app/blocks/feature/FeatureTwo.tsx
@@ -1,45 +1,69 @@
 import Image from "next/image";
-import ButtonLink from "../../components/Button";
+import ButtonLink from "../../components/ButtonLink";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
 
-export default function FeatureTwoBlock() {
-  const content = {
-    byline: "This is a byline",
-    headline: "This is a headline",
-    copy: "Copy - Donec vel auctor augue. Nulla metus sem, pellentesque ut cursus nec, dapibus ac arcu. Vivamus erat massa, posuere sit amet orci at, viverra hendrerit sem, sed ut lacus in nulla posuere scelerisque.",
-    cta: {
-      url: "#",
-      text: "Learn more",
-    },
-    image: {
-      url: "https://hwhfyb8e9b.ufs.sh/f/E8jMENC68tjA4lvPTjxfshF9qjNVRickg0ZzuwdolY726EL5",
-      alt: "Image alt",
-    },
+interface FeatureTwoBlockProps {
+  theme?: "light" | "dark";
+  byline: string;
+  headline: string;
+  children: React.ReactNode;
+  mediaDirection?: "left" | "right";
+  cta: {
+    url: string;
+    text: string;
   };
+  img: {
+    width: number;
+    height: number;
+    url: string;
+    alt: string;
+  };
+}
+
+export default function FeatureTwoBlock({
+  theme = "dark",
+  byline = "",
+  headline,
+  children,
+  mediaDirection = "right",
+  cta,
+  img,
+}: FeatureTwoBlockProps) {
   return (
-    <div className="container ">
-      <div className="grid grid-cols-1 border border-theme-weak bg-theme-filling rounded-theme-lg md:grid-cols-2">
-        <div className="flex flex-col items-start justify-center px-4 py-8 lg:px-8 lg:py-12">
-          <p className="mb-1 font-semibold body-md text-theme-strong">{content.byline}</p>
-          <h2 className="mb-3 lg:mb-4 head-h3 text-theme-heading">{content.headline}</h2>
-          <div className="body-lg text-theme-heading">{content.copy}</div>
-          <div className="mt-8">
-            <ButtonLink href="#" type="solid" theme="dark">
-              Hello
-            </ButtonLink>
+    <section data-theme={theme}>
+      <div className="bg-theme-000 dark:bg-theme-900 ">
+        <div className="container py-8 lg:py-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 mx-auto max-w-5xl rounded-2xl overflow-hidden bg-theme-800 border border-theme-600">
+            <div
+              className={clsx(
+                mediaDirection == "left" ? "order-first" : "order-last",
+                "order h-full"
+              )}
+            >
+              <Image
+                width={img.width}
+                height={img.height}
+                src={img.url}
+                alt={img.alt}
+                className="min-h-full aspect-[4/3] w-full object-cover"
+                loading="lazy"
+              />
+            </div>
+            <div className="flex flex-col items-start justify-center p-4 md:p-6 lg:p-8">
+              <p className="mb-1 font-semibold text-lg text-theme-300">{byline}</p>
+              <h2 className="font-heading font-bold text-4xl mb-2 text-theme-000">{headline}</h2>
+              <div className="text-lg/relaxed text-theme-300 ">{children}</div>
+              <div className="mt-4">
+                <ButtonLink href={cta.url} type="solid" theme="dark">
+                  {cta.text}
+                  <ArrowRightIcon className="size-5" />
+                </ButtonLink>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className="order-last h-full p-6">
-          <Image
-            width="640"
-            height="480"
-            src={content.image.url}
-            alt={content.image.alt}
-            className="min-h-full aspect-[4/3] w-full object-cover rounded-theme-md"
-            loading="lazy"
-          />
-        </div>
       </div>
-    </div>
+    </section>
   );
 }
