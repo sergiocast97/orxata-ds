ce6e74db387b9bfdcd05f7ce4e512524832a3d84
Sergio <sergiocast97@gmail.com>
2025-02-19 00:25:06 +0000

    Hero Three content

--- src/app/blocks/hero/HeroThree.tsx
+++ src/app/blocks/hero/HeroThree.tsx
@@ -2,35 +2,53 @@ import ButtonLink from "@/app/components/Button";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-export default function HeroThreeBlock() {
+interface HeroThreeBlockProps {
+  byline: string;
+  headline: string;
+  copy: string;
+  cta: {
+    text: string;
+    url: string;
+  };
+  img: {
+    url: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+}
+
+export default function HeroThreeBlock({
+  byline = "",
+  headline = "",
+  copy = "",
+  cta,
+  img,
+}: HeroThreeBlockProps) {
   return (
     <div className="bg-theme-background" data-theme="dark">
-      <div className="p-6 md:p-8 lg:p-12 xl:p-20">
-        <div className="container">
-          <Image
-            width="1312"
-            height="480"
-            src="https://hwhfyb8e9b.ufs.sh/f/E8jMENC68tjA4lvPTjxfshF9qjNVRickg0ZzuwdolY726EL5"
-            alt="Hero image"
-            className="aspect-[7/3] w-full object-cover rounded-theme-lg"
-            loading="lazy"
-          />
-          <div className="grid grid-cols-1 xl:grid-cols-2 pt-8 gap-8">
-            <div className="flex flex-col items-start justify-center gap-1">
-              <p className="body-sm font-semibold text-theme-strong">Lorem Ipsum dolor sit amet</p>
-              <h1 className="head-h1 text-theme-heading text-balance">
-                Impactful Headline about product
-              </h1>
-            </div>
-            <div className="flex flex-col items-start justify-center gap-8">
-              <p className="body-lg text-theme-copy max-w-[48ch]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra eros bibendum
-                sapien sagittis, ac efficitur felis accumsan. Vivamus ut efficitur risus.
-              </p>
-              <ButtonLink href="#" type="solid" theme="light">
-                Learn more <ArrowRightIcon className="size-6" />
-              </ButtonLink>
-            </div>
+      <div className="container py-8 lg:pt-12 pb-24">
+        <Image
+          width={img.width}
+          height={img.height}
+          src={img.url}
+          alt={img.alt}
+          className="aspect-[3/2] sm:aspect-[2/1] lg:aspect-[3.25/1] w-full object-cover rounded-2xl"
+          loading="lazy"
+        />
+        <div className="grid grid-cols-1 lg:grid-cols-2 pt-6 xl:pt-8 gap-4 lg:gap-6 xl:gap-8">
+          <div className="flex flex-col items-start justify-center gap-1">
+            <p className="text-lg/relaxed font-semibold text-theme-strong">{byline}</p>
+            <h1 className="font-heading font-bold text-5xl lg:text-6xl text-theme-heading text-balance">
+              {headline}
+            </h1>
+          </div>
+          <div className="flex flex-col items-start justify-center gap-6">
+            <p className="text-lg/relaxed text-theme-copy max-w-[48ch]">{copy}</p>
+            <ButtonLink href={cta.url} type="solid" theme="light">
+              {cta.text}
+              <ArrowRightIcon className="size-6" />
+            </ButtonLink>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,18 @@ import HeroThreeBlock from "./blocks/hero/HeroThree";
 export default function Home() {
   return (
     <>
-      <HeroThreeBlock />
+      <HeroThreeBlock
+        byline="Lorem ipsum dolor sit amet"
+        headline="Impactful Headline about Product"
+        copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra eros bibendum sapien sagittis, ac efficitur felis accumsan. Vivamus ut efficitur risus."
+        cta={{ text: "Learn more", url: "#" }}
+        img={{
+          width: 1280,
+          height: 360,
+          url: "https://hwhfyb8e9b.ufs.sh/f/E8jMENC68tjA4lvPTjxfshF9qjNVRickg0ZzuwdolY726EL5",
+          alt: "Hero Image",
+        }}
+      />
     </>
   );
 }
